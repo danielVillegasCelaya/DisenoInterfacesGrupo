776cbc0dc3407180036896f84c678cf5132ec4aa
DiegoGonzy013 <gonzy0.13heybrother@gmail.com>
2022-02-09 13:46:00 +0100

    No se lo que hago xd

---
+++ my-app/src/data/menuNav.js
@@ -0,0 +1,19 @@
+export const MenuItems = [
+    {
+      id: 1,
+      path: '/login',
+      title: 'Login',
+      component: Login,
+    },
+    {
+      id: 2,
+      path: '/api',
+      title: 'Api',
+      component: Api,
+    },
+    {
+      id: 3,
+      path: '/inicio',
+      title: 'Inicio',
+      component: Inicio,
+    }];
