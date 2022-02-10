92a1694863f529a9a1113983765a8cda32e34da4
DiegoGonzy013 <boria37diego@gmail.com>
2022-02-10 14:03:49 +0100

    F COMMIT

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import MenuNavBar from './componentes/MenuNavBar';
+import {MenuNav} from './data/MenuNav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.rty
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <MenuNavBar />
+        </header>
+        {MenuNav.map((item) => {
+          return (
+            <Route
+              key={item.id}
+              path={item.path}
+              exact
+              component={item.component}/>
+            );
+        })}
+      </div>
+    </Router>
   );
 }
 
--- my-app/src/componentes/menuNavBar.jsx
+++ my-app/src/componentes/menuNavBar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Container, Nav, NavDropdown,Form } from 'react-bootstrap';
-import { menuNavBar } from '../data/menuNavBar';
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl,Button } from 'react-bootstrap';
+import { MenuNav } from '../data/MenuNav';
 
-class menuNavBar extends React.Component {
+class MenuNavBar extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="Dark" expand="lg">
         <Container fluid>
           <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
@@ -20,36 +20,28 @@ class menuNavBar extends React.Component {
               style={{ maxHeight: '100px' }}
               navbarScroll
             >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
+              <Nav.Link href="">Home</Nav.Link>
+              <Nav.Link href="https://www.afuegolento.com/">Recetas</Nav.Link>
               <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                {menuNav.map((item) => {
+                {MenuNav.map((item) => {
                   return <Nav.Link as={Link} to={item.path}>
                     {item.title}</Nav.Link>
                 })}
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Buscar"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Buscar</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+      </Navbar >
     );
   }
 }
-export default menuNavBar;
+export default MenuNavBar;
