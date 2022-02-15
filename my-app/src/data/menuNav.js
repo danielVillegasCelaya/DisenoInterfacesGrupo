fe5a6faf6031ee89a966219b154ffea8eaff2ada
DiegoGonzy013 <gonzy0.13heybrother@gmail.com>
2022-02-15 11:55:10 +0100

    resuelvo errores

--- my-app/src/data/menuNav.js
+++ my-app/src/data/menuNav.js
@@ -1,19 +1,23 @@
+import ApiOnline from '../components/ApiOnline';
+import Login from '../components/Login';
+import Inicio from '../components/Inicio';
+
 export const MenuNav = [
-    {
-      id: 1,
+  {
+    id: 1,
+    path: '/api',
+    title: 'Api',
+    component: ApiOnline,
+  },  
+  {
+      id: 2,
       path: '/login',
       title: 'Login',
       component: Login,
-    },
-    {
-      id: 2,
-      path: '/api',
-      title: 'Api',
-      component: Api,
     },
     {
       id: 3,
       path: '/inicio',
       title: 'Inicio',
       component: Inicio,
-    }];
+    }*/];
