f6c76317078af44b1544080974ab505eeedd197a
DiegoGonzy013 <gonzy0.13heybrother@gmail.com>
2022-02-15 12:09:59 +0100

    componentes

---
+++ my-app/src/componentes/Inicio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+class Inicio extends React.Component {
+    constructor(props) {
+        super(props); 
+    }
+
+render() {
+    return (
+        <div className="main-site">
+        Esto es el inicio
+        </div>
+    );
+}
+}
+export default Inicio;
---
+++ my-app/src/componentes/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+class Login extends React.Component {
+    constructor(props) {
+        super(props); 
+    }
+
+render() {
+    return (
+        <div>
+        Esto es el login
+        </div>
+    );
+}
+}
+export default Login;
