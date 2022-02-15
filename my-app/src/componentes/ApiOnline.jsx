c0284a5955fce3a541805305ecbfe0c72459ecc1
DiegoGonzy013 <gonzy0.13heybrother@gmail.com>
2022-02-15 12:16:47 +0100

    f

---
+++ my-app/src/componentes/ApiOnline.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ApiOnline extends React.Component {
+    constructor(props) {
+        super(props); 
+    }
+
+render() {
+    return (
+        <div className="main-site">
+        Esto es el la api
+        </div>
+    );
+}
+}
+export default ApiOnline;
