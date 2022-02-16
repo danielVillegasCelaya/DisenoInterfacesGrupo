72b53021232060c68e3e916af2c7e4df42928b87
DiegoGonzy013 <gonzy0.13heybrother@gmail.com>
2022-02-16 14:06:50 +0100

    fgdf

--- my-app/src/componentes/menuNavBar.jsx
+++ my-app/src/componentes/menuNavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav,Offcanvas ,Form, FormControl, Button } from 'react-bootstrap';
 import Inicio from '../componentes/Inicio';
 import { MenuNav } from '../data/MenuNav';
 
