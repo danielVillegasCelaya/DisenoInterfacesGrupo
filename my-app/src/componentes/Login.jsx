928d97d7072d7c560558e99a93137c403247382f
Eduardo <ecardenosag01@educantabria.es>
2022-02-26 17:39:13 +0100

    commit de login

--- my-app/src/componentes/Login.jsx
+++ my-app/src/componentes/Login.jsx
@@ -1,16 +1,112 @@
-import React from "react";
+import React from 'react';
+import { Container, Form, Button, Row, Col, FloatingLabel } from 'react-bootstrap';
+import { Usuarios } from '../data/Usuarios';
+import './index.css';
 
-class Login extends React.Component{
-    constructor(props){
-        super(props)
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { Usuario: null };
+        this.users = Usuarios;
+        this.login = this.login.bind(this);
+        this.inputUser = React.createRef();
+        this.inputPassword = React.createRef();
     }
-    render(){
-        return(
-            <>
-            <h1>Soy el login</h1>
-            </>
-        )
+
+    login() {
+
+        let e = false;
+
+        this.users.map((item) => {
+            if (item.user.match(this.inputUser.current.value) && item.password.match(this.inputPassword.current.value)) {
+                e = true;
+                this.setState({ Usuario: item });
+            }
+        });
+
+        if (e == true) {
+            alert('Bienvenido');
+        } else {
+            alert('Usuario o contreña incorrectos.');
+        }
     }
-   
+
+
+    componentDidMount() {
+
+        if (localStorage.getItem('id') !== 'null') {
+            this.setState({
+                Usuario: this.users[localStorage.getItem('id')]
+            });
+        } else {
+            this.setState({
+                Usuario: null
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.state.Usuario !== null) {
+            localStorage.setItem('id', this.state.Usuario.id);
+        } else {
+            localStorage.setItem('id', null);
+        }
+    }
+
+
+
+    render() {
+        if (this.state.Usuario !== null) {
+            return (
+                <div className="bg">
+                    <br />
+                    <Container>
+                        <Row>
+                            <Col xs={8} className="p-3 m-auto  shadow rounded">
+                                <h1>Bienvenido {this.state.Usuario.user}</h1>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            );
+        } else {
+
+            return (
+                <div className="bg">
+                    <br />
+                    <Container>
+                        <Row>
+                            <Col xs={3} className="p-3 m-auto  shadow rounded">
+
+                                <Form>
+                                    <FloatingLabel controlId="floatingInput" label="Usuario" className="mb-3">
+                                        <Form.Control size='lg'
+                                            type="email"
+                                            placeholder="Usuario"
+                                            ref={this.inputUser} />
+                                    </FloatingLabel>
+
+                                    <FloatingLabel controlId="floatingPassword" label="Contraseña" className="mb-3">
+                                        <Form.Control size='lg'
+                                            type="password"
+                                            placeholder="Contraseña"
+                                            ref={this.inputPassword} />
+                                    </FloatingLabel>
+                                </Form>
+
+                                <div className="d-grid gap-2">
+                                    <Button size='lg' variant="primary" type="button" onClick={this.login}>
+                                        Iniciar Sesion
+                                    </Button>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            );
+        }
+    }
 }
+
 export default Login;
